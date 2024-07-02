0b829e2da248566fd34e979ea11ec390cc8573bc
ravinravi03 <anuravinithin@gmail.com>
2024-07-02 22:25:15 +1200

    Create a user when a new user logs in

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import passport from 'passport';
 import { Strategy as SpotifyStrategy } from 'passport-spotify';
+import User from '../models/userModel.js';
 
 dotenv.config();
 
@@ -15,8 +16,25 @@ passport.use(
             callbackURL: 'http://localhost:5050/login/spotify/callback',
             scope: ['playlist-modify-public', 'playlist-modify-private'],
         },
-        function(accessToken,refreshToken,expires_in, profile, done){
-            return done(null,{profile, accessToken, refreshToken});
+        async (accessToken, refreshToken, expires_in, profile, done) => {
+            try {
+                let user = await User.findOne({ id: profile.id });
+
+                if (!user) {
+                    user = new User({
+                        display_name: profile.displayName,
+                        id: profile.id,
+                        profile_picture: profile.photos ? profile.photos[1].value : null,
+                    });
+                    await user.save();
+                }
+
+                return done(null, {profile, accessToken, refreshToken,});
+
+            } catch (error) {
+                console.error('Error processing Spotify authentication:', error);
+                return done(error);
+            }
         }
 
     )
