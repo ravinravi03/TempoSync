053807676d8de93c8c7707b299149c68497c78ad
ravinravi03 <anuravinithin@gmail.com>
2024-07-11 21:48:22 +1200

    Set cors credentials to true for sharing cookies from backend to frontend

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5050;
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+  origin: `${process.env.FRONTEND_URL}`,
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/login',loginRouter);
